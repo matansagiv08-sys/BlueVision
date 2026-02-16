9a4b817f12c8572291068ecacf75e0da60245199
LinorCohen3 <Linor33330@gmail.com>
2026-02-16 10:48:01 +0200

    Login1

---
+++ Client/JS/Login.js
@@ -0,0 +1,11 @@
+﻿document.getElementById('loginForm').addEventListener('submit', function (e) {
+    e.preventDefault(); // מונע מהדף להתרענן
+
+    const user = document.getElementById('username').value;
+    const pass = document.getElementById('password').value;
+
+    console.log("ניסיון התחברות עבור:", user);
+
+    // כאן תוכל להוסיף קריאת API לשרת שלך
+    alert('נשלח ניסיון התחברות עבור המשתמש: ' + user);
+});
