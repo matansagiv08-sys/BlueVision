2411ab76a940af9950bc9b9d10a620517b5f964f
LinorCohen3 <Linor33330@gmail.com>
2026-03-14 13:46:21 +0200

    model and controller of Item in production

---
+++ Server/Controllers/ItemsInProductionController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Server.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ItemsInProductionController : ControllerBase
+    {
+        // GET: api/<ItemsInProductionController>
+        [HttpGet]
+        public IEnumerable<ItemsInProduction> Get()
+        {
+            ItemsInProduction item = new ItemsInProduction();
+            return item.Read();
+        }
+
+        // POST api/<ItemsInProductionController>
+        [HttpPost]
+        public int Post([FromBody] ItemsInProduction item)
+        {
+            int numEffected = item.Insert();
+            return numEffected;
+        }
+    }
+}
---
+++ Server/Models/ItemsInProduction.cs
@@ -0,0 +1,48 @@
+﻿using Server.DAL;
+
+namespace Server.Models
+{
+    public class ItemsInProduction
+    {
+        int serialNumber;
+        int productItemID;
+        int planeID;
+        int priorityLevel;
+        int workOrderID;
+        int plannedQty;
+        string comments;
+
+        public int SerialNumber { get => serialNumber; set => serialNumber = value; }
+        public int ProductItemID { get => productItemID; set => productItemID = value; }
+        public int PlaneID { get => planeID; set => planeID = value; }
+        public int PriorityLevel { get => priorityLevel; set => priorityLevel = value; }
+        public int WorkOrderID { get => workOrderID; set => workOrderID = value; }
+        public int PlannedQty { get => plannedQty; set => plannedQty = value; }
+        public string Comments { get => comments; set => comments = value; }
+
+        public ItemsInProduction() { }
+
+        public ItemsInProduction(int serialNumber, int productItemID, int planeID, int priorityLevel, int workOrderID, int plannedQty, string comments)
+        {
+            SerialNumber = serialNumber;
+            ProductItemID = productItemID;
+            PlaneID = planeID;
+            PriorityLevel = priorityLevel;
+            WorkOrderID = workOrderID;
+            PlannedQty = plannedQty;
+            Comments = comments;
+        }
+
+        public int Insert()
+        {
+            DBservices dbs = new DBservices();
+            return dbs.Insert(this);
+        }
+
+        public List<ItemsInProduction> Read()
+        {
+            DBservices dbs = new DBservices();
+            return dbs.ReadItemsInProduction(); // נניח שזו המתודה שתצרי ב-DBservices
+        }
+    }
+}
